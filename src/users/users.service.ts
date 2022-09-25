9904663647d54392cee99156caf803819586be98
tahuuhao <hao.th@zinza.com.vn>
2022-09-25 16:59:36 +0700

    Alter package bcrypt to bcryptjs

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -4,7 +4,7 @@ import { Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from 'src/typeorm/entities/user.entity';
 import { IUserResponse } from './interfaces/user.interface';
-import * as bcrypt from 'bcrypt';
+import * as bcrypt from 'bcryptjs';
 import { UpdateUserDto } from './dto/update-user.dto';
 @Injectable()
 export class UsersService {
