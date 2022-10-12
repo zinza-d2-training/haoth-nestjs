7b9825eee7060083ca59aeb28ba3fdf223bb24cc
tahuuhao <hao.th@zinza.com.vn>
2022-10-12 16:57:25 +0700

    Implement API User

--- src/models/users/users.module.ts
+++ src/models/users/users.module.ts
@@ -1,11 +1,18 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/typeorm/entities/user.entity';
+import { jwtConstants } from '../auth/constants';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    JwtModule.register({
+      secret: jwtConstants.secret,
+    }),
+  ],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService],
